bae97e3ab4927f947dede39c811a1134a30f314c
oldirtychi <ejsapayanp@gmail.com>
2024-07-26 14:31:45 -0500

    Part 1

--- js/index.mjs
+++ js/index.mjs
@@ -9,12 +9,7 @@ const n4 = 5;
 // arithmetic operators and a comparison.
 // const isSum50 = (n1 + n2 + n3 + n4) == 50;
 
-const allDivisibleBy5 = (n1 % 5 == 0) && (n2 % 5 == 0) && (n3 % 5 == 0) && (n4 % 5 == 0);
 
-
-// const firstLargerThanLast = n1 > n4;
-
-// const arithmeticChain = (n2 - n1)*n3 % n4;
 // // Check two: at least two odd numbers
 // // Here, we use modulus to check if something is odd.
 // // Since % 2 is 0 if even and 1 if odd, we can use
@@ -49,3 +44,32 @@ const allDivisibleBy5 = (n1 % 5 == 0) && (n2 % 5 == 0) && (n3 % 5 == 0) && (n4 %
 //   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
 //   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
 //   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+//ALAB-308.1.1
+
+
+//PART 1
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const allDivisibleBy5 = n1 % 5 == 0 && n2 % 5 == 0 && n3 % 5 == 0 && n4 % 5 == 0;
+
+// Check if the first number is larger than the last. Cache the result in a variable.
+const firstLargerThanLast = n1 > n4;
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+const arithmeticChain = (n2 - n1) * n3 % n4;
+
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+const isNotOver25 = !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25);
+
+console.log(allDivisibleBy5)
+
+console.log(arithmeticChain)
+
+
+
